9627afb883f1c6d4c094a882611d6e753ef0645a
Antoine <antoine.geiger0038@stud.hkr.se>
2024-03-03 19:30:03 +0100

    changed HighScore to Score

--- Players/Player.py
+++ Players/Player.py
@@ -4,26 +4,30 @@ class Player():
     nickname = ""
     balance = 0
 
-    # dictionary value: player nickname, key: balance
-    players = {}
 
-    """ initialise player object """
-    def __init__(self, nickname, balance=0):
+    def __init__(self, nickname, scores:Scores, balance=0):
+    """ initialise player object with Scores object """
+
         self.nickname = nickname
         self.balance = balance
+        self.scores = scores
+
         # add player to dictionary with initial balance
+        scores.add
         self.players[nickname] = balance
 
 
-    """ add new player to players """
     @classmethod
     def addPlayer(cls, nickname, balance=0):
+    """ add new player to players """
+
         if nickname not in cls.players:
             cls.players[nickname] = balance
     
-    """ update player balance after gain/loss """
     @classmethod
     def updateBalance(cls, nickname, newBalance):
+    """ update player balance after gain/loss """
+
         # if nickname exists in players dict
         if nickname in cls.players:
             # update balance
@@ -31,6 +35,7 @@ class Player():
         # if not add him
         else:
             cls.addPlayer(nickname, newBalance)
+
         return cls.players
 
 p = Player("Tibor", 0)
