41e980a8f3056777de1cace2044feaa464ed79f8
Kate Arvay <kate@arvay.cz>
2024-03-06 12:30:16 +0100

    test cardHigher

---
+++ Tests/test_bet.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append(".")
+
+import unittest
+from GameMechanics import Bet as betClass
+
+class TestBet(unittest.TestCase):
+
+    def testCardHigher(self):
+        """ Test if balance updated after win """
+        # assign test values
+        currentBalance = 1000
+        bet = 10
+        # if card higher (win) -> win double the bet
+        expectedBalance = currentBalance + bet
+        # instantiate class
+        betInstance = betClass.Bet()
+        # call the method from the class
+        updatedBalance = betInstance.cardHigher(currentBalance, bet)
+        # make sure the expected balance and class method output same
+        self.assertEqual(updatedBalance, expectedBalance)
+
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
