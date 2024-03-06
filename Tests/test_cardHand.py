1f7a930eb23a9196635f690f2d25ba92431ba6ac
Kate Arvay <kate@arvay.cz>
2024-03-06 19:00:34 +0100

    test drawCard

--- Tests/test_cardHand.py
+++ Tests/test_cardHand.py
@@ -21,6 +21,25 @@ class TestCardHand(unittest.TestCase, deckClass.Deck, cardClass.Card):
         self.assertEqual(cardHandInstance.shuffledDeck, testDeck)
 
 
+    """Test Draws one card for the player and one for the AI and removes those cards from the deck"""
+    def testDrawCard(self):
+        # create test deck object
+        testDeck = {"heart": 2, "diamond": 3 ,"club": 5}
+        # create test deck copy to retain the original object
+        originalTestDeck = testDeck.copy()
+        # create instance of the cardHand class, pass the test deck copy
+        cardHandInstance = cardHandClass.CardHand(testDeck.copy())
+        # draw card for player and ai
+        expectedPlayerHand = originalTestDeck.popitem()
+        expectedAiHand = originalTestDeck.popitem()
+        # call the method from the class
+        playerHandOutput, aiHandOutput, deckOutput = cardHandInstance.drawCard(testDeck.copy())
+        # make sure the expected ouput from test and class method same
+        self.assertEqual(playerHandOutput, expectedPlayerHand) 
+        self.assertEqual(aiHandOutput, expectedAiHand)
+        self.assertEqual(deckOutput,originalTestDeck)
+
+
 
 
 if __name__ == '__main__':
