c067eeb38b95410380475538922cbc34f6d17785
Kate Arvay <kate@arvay.cz>
2024-03-06 12:47:44 +0100

    test cardLower

--- Tests/test_bet.py
+++ Tests/test_bet.py
@@ -20,6 +20,20 @@ class TestBet(unittest.TestCase):
         # make sure the expected balance and class method output same
         self.assertEqual(updatedBalance, expectedBalance)
 
+    def testCardLower (self):
+        """ Test if balance updated after loss """
+        # assign test values
+        currentBalance = 1000
+        bet = 10
+        # if card higher (win) -> win double the bet
+        expectedBalance = currentBalance - bet
+        # instantiate class
+        betInstance = betClass.Bet()
+        # call the method from the class
+        updatedBalance = betInstance.cardLower(currentBalance, bet)
+        # make sure the expected balance and class method output same
+        self.assertEqual(updatedBalance, expectedBalance)
+
         
 
 
