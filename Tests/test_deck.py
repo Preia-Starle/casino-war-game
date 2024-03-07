2948f41ac99b1415758f94a7abd5e6d0a6d26d0e
Kate Arvay <kate@arvay.cz>
2024-03-07 12:58:10 +0100

    test Deck constructor

---
+++ Tests/test_deck.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append(".")
+
+import unittest
+from CardMechanics import Card as cardClass
+from CardMechanics import Deck as deckClass
+
+class TestDeck(unittest.TestCase, cardClass.Card):
+
+    def test__init__deck(self):
+        """ Test constructor """
+        # create test card object
+        cardInstance = cardClass.Card()
+        cardInstance.testCards = {
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9,
+            "10": 10,
+            "Jack": 11,
+            "Queen": 12,
+            "King": 13,
+            "Ace": 14}
+        cardInstance.testSuits = ["Diamonds", "Hearts", "Clubs", "Spades"]
+        # call the constructor and pass the test card object
+        deckInstance = deckClass.Deck(cardInstance)
+        # make sure deck and shuffledDeck properly initialised
+        self.assertEqual(deckInstance.deck, {})
+        self.assertEqual(deckInstance.shuffledDeck, {}) 
+        # make sure card object with cards and suits gets successfully passed
+        self.assertEqual(deckInstance.cards, cardInstance.testCards)
+        self.assertEqual(deckInstance.suits, cardInstance.testSuits)
+
